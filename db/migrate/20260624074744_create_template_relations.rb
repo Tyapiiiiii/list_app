ba7169af3aa42bbaf60dcaff64a77988cc0d6b66
Tyapiiiiii <halloween.fu@icloud.com>
2026-06-24 16:55:37 +0900

    マイグレーションファイルの修正（初期値の設定）

--- db/migrate/20260624074744_create_template_relations.rb
+++ db/migrate/20260624074744_create_template_relations.rb
@@ -3,9 +3,12 @@ class CreateTemplateRelations < ActiveRecord::Migration[7.2]
     create_table :template_relations do |t|
       t.references :user, null: false, foreign_key: true
       t.references :template, null: false, foreign_key: true
-      t.integer :status
+      # statusに 初期値default: 0 (pending) と null: false を追加します
+      t.integer :status, null: false, default: 0
 
       t.timestamps
     end
+    # 同じユーザーが同じテンプレートに2回以上招待されないようにユニーク制約をつけておく
+    add_index :template_relations, [:user_id, :template_id], unique: true
   end
 end
