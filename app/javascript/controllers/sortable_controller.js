131ae4e8abc05e6fb1cb422287084cec640dae0a
Tyapiiiiii <halloween.fu@icloud.com>
2026-06-24 16:42:00 +0900

    ドラッグ＆ドロップ修正

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -1,18 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 import Sortable from "sortablejs"
 
-// Connects to data-controller="sortable"
 export default class extends Controller {
   connect() {
     this.sortable = Sortable.create(this.element, {
       animation: 150,
-      handle: ".handle", // つまみ（ハンドル）となる要素のクラス名
+      handle: ".handle",
       onEnd: this.end.bind(this)
     })
   }
 
   end(event) {
-    // ここでドラッグが終わった後に、新しい並び順を裏側で保存する処理を後ほど追加します
-    console.log("並び替えられたよ！", event.oldIndex, "から", event.newIndex)
+    // ドラッグが終わったら、並び替わった要素内のすべての「hidden_field」を上から順に取得
+    const inputs = this.element.querySelectorAll(".position-input")
+    
+    // 上から順番に 1, 2, 3... と数値を上書きしていく
+    inputs.forEach((input, index) => {
+      input.value = index + 1
+    })
+    
+    console.log("フォーム内の並び順番号を更新しました！")
   }
 }
