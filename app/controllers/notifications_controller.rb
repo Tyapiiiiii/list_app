32f819e0bcd45a762f0e16a40cd388e965564a54
Tyapiiiiii <halloween.fu@icloud.com>
2026-06-25 14:50:44 +0900

    通知機能の修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,5 +2,8 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @pending_invitations = current_user.template_relations
+                                       .where(status: :pending)
+                                       .includes(template: :user)
   end
 end
