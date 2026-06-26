637fc9d510103a6d63a4388b0f7e68b038aabc9a
Tyapiiiiii <halloween.fu@icloud.com>
2026-06-26 13:08:07 +0900

    デバック

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,11 +2,9 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!
 
   def toggle_check
-    @item = Item.find(params[:id])
-    # 現在のチェック状態を反転させて保存
+    @item = accessible_items.find(params[:id])
     @item.update(is_checked: !@item.is_checked)
 
-    # Turboを使って、チェックを入れた部分だけを画面更新する
     respond_to do |format|
       format.turbo_stream
       format.html { redirect_to template_path(@item.template) }
@@ -14,16 +12,25 @@ class ItemsController < ApplicationController
   end
 
   def move
-    # ログインユーザーが所有するテンプレートのアイテムであることを確認しつつ取得
-    @item = current_user.templates.flat_map(&:items).find { |i| i.id == params[:id].to_i }
+    @item = owned_items.find_by(id: params[:id])
 
     if @item
-      # acts_as_listのメソッドを使って、指定された位置に移動
-      # JavaScriptからは 0から始まるインデックスが届くので +1 
       @item.insert_at(params[:position].to_i + 1)
       head :no_content
     else
       head :not_found
     end
+  end
+
+  private
+
+  def accessible_items
+    owned  = Item.where(template_id: current_user.templates.select(:id))
+    shared = Item.where(template_id: current_user.shared_templates.select(:id))
+    owned.or(shared)
+  end
+
+  def owned_items
+    Item.where(template_id: current_user.templates.select(:id))
   end
 end
